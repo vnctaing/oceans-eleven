d0c6c7653978379a4c9672f78094b9fbb49b0f4b
Vincent Taing <taing.vincent@gmail.com>
2020-05-21 18:25:03 -0700

    Add get-trips create-trip datomic transaction fn

--- src/clj/oceans_eleven/db/core.clj
+++ src/clj/oceans_eleven/db/core.clj
@@ -3,31 +3,30 @@
    [datomic.client.api :as d]
    [io.rkn.conformity :as c]
    [mount.core :refer [defstate]]
+   [clojure.pprint :as p]
+   [ring.util.request]
    [oceans-eleven.config :refer [env]]))
-
-
-(defn install-schema
-  "This function expected to be called at system start up.
-
-  Datomic schema migraitons or db preinstalled data can be put into 'migrations/schema.edn'
-  Every txes will be executed exactly once no matter how many times system restart."
-  [conn]
-  (let [norms-map (c/read-resource "migrations/schema.edn")]
-    (c/ensure-conforms conn norms-map (keys norms-map))))
 
 (def cfg {:server-type        :peer-server
           :access-key         "myaccesskey"
           :secret             "mysecret"
           :endpoint           "localhost:8998"
           :validate-hostnames false})
+
 (def client (d/client cfg))
 
 (def conn (d/connect client {:db-name "pensine"}))
 
-(def o11-schema [
-                 {:db/ident       :trip/name
+(def o11-schema [{:db/ident       :trip/name
                   :db/valueType   :db.type/string
-                  :db/cardinality :db.cardinality/one}]
-  )
+                  :db/cardinality :db.cardinality/one}])
 
-(d/transact conn {:tx-data o11-schema })
+(defn create [e]
+  (let [body-str  (ring.util.request/body-string e)
+        trip-name (:params e)]
+    (d/transact conn {:tx-data [{:trip/name (:name trip-name)}]})))
+
+(def all-trips '[:find ?e
+                 :where [_ :trip/name ?e]])
+(defn get-all [e]
+  (d/q {:query all-trips :args [ (d/db conn) ]}))
