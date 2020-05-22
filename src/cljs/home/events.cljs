0ccb585c3ad5f2613c7e9250e838a0becd775791
Vincent Taing <taing.vincent@gmail.com>
2020-05-21 18:34:41 -0700

    Create event-fx for getting and creating trip

--- src/cljs/home/events.cljs
+++ src/cljs/home/events.cljs
@@ -5,13 +5,23 @@
    [ajax.core :as ajax]))
 
 (rf/reg-event-fx
- :create-trip
- (fn [_ [_ a]]
-   {:http-xhrio {:method :post
-                 :uri "http://localhost:3000/trip/create"
-                 :timeout 5000
-                 :format          (ajax/json-request-format)
-                 :response-format (ajax/json-response-format {:keywords? true})
-                 :on-success []
-                 :on-failure [:common/set-error]
-                 }}))
+  :create-trip
+  (fn [_ [_ name]]
+    {:http-xhrio {:method          :post
+                  :uri             "http://localhost:3000/trip/create"
+                  :params          {:name name}
+                  :timeout         5000
+                  :format          (ajax/json-request-format)
+                  :response-format (ajax/json-response-format {:keywords? true})
+                  :on-success      []
+                  :on-failure      [:common/set-error]}}))
+
+(rf/reg-event-fx
+  :get-trips
+  (fn [_ _]
+    {:http-xhrio {:method          :get
+                  :uri             "http://localhost:3000/trips"
+                  :timeout         5000
+                  :response-format (ajax/json-response-format {:keywrds? true})
+                  :on-success      [:common/set-error]
+                  :on-failure      [:common/set-error]}}))
