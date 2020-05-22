6381ef3359f590a4a34d8c722eb29bd95c53b1d1
Vincent Taing <taing.vincent@gmail.com>
2020-05-21 18:33:13 -0700

    Add /trip/create and /trips api route

--- src/clj/oceans_eleven/routes/home.clj
+++ src/clj/oceans_eleven/routes/home.clj
@@ -1,21 +1,20 @@
 (ns oceans-eleven.routes.home
   (:require
-    [oceans-eleven.layout :as layout]
-    [clojure.java.io :as io]
-    [oceans-eleven.middleware :as middleware]
-    [ring.util.http-response :as response]))
+   [oceans-eleven.layout :as layout]
+   [clojure.java.io :as io]
+   [oceans-eleven.middleware :as middleware]
+   [oceans-eleven.db.core :as db]
+   [ring.util.http-response :as response]))
 
 (defn home-page [request]
   (layout/render request "home.html"))
 
 (defn home-routes []
   [""
-   {:middleware [middleware/wrap-csrf
+   {:middleware [;; middleware/wrap-csrf
                  middleware/wrap-formats]}
    ["/" {:get home-page}]
    ["/graphiql" {:get (fn [request] (layout/render request "graphiql.html"))}]
-   ["/trip/create" {:post (fn [request] (response/ok))}]
-   ["/docs" {:get (fn [_]
-                    (-> (response/ok (-> "docs/docs.md" io/resource slurp))
-                        (response/header "Content-Type" "text/plain; charset=utf-8")))}]])
+   ["/trip/create" {:post (fn [request] (response/ok (db/create request)))}]
+   ["/trips" {:get (fn [request]  (response/ok (db/get-all request)))}]])
 
